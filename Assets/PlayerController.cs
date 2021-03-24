9f395039f7040d6097dd22f3ddde775aaf01ef19
Jaideep Sidhu <37817565+jsidhu26@users.noreply.github.com>
2021-03-24 15:16:45 -0400

    fix player rotation, add GUI controls (run, jump)

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,38 +6,52 @@ using UnityStandardAssets.CrossPlatformInput;
 // This script will handle controlling the main character
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody2D rb;
+    [SerializeField]private Rigidbody2D rb;
+    [SerializeField]private Animator anim;
     
-    
-    // private float dirX;
+    private float hDirection;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        hDirection = CrossPlatformInputManager.GetAxis("Horizontal");
+
         // If the player hits the 'A' key
-        if (Input.GetKey(KeyCode.A))
+        if (hDirection < 0)
         {
             // Change the velocity of the player to move to the left by 5
             rb.velocity = new Vector2(-5, rb.velocity.y);
-            transform.localScale = new Vector2(-1, 1);
+            // Flip the sprite so its facing the left
+            transform.localScale = new Vector2(-1f, 1f);
+            // Start the running animation
+            anim.SetBool("running", true);
         }
         
         // If the player hits the 'D' key
-        if (Input.GetKey(KeyCode.D))
+        else if (hDirection > 0)
         {
             // Change the velocity of the player to move to the right by 5
             rb.velocity = new Vector2(5, rb.velocity.y);
-            transform.localScale = new Vector2(1, 1);
+            // Flip the sprite so its facing the right
+            transform.localScale = new Vector2(1f, 1f);
+            // Start the running animation
+            anim.SetBool("running", true);
+        }
+
+        // If no key is pressed
+        else{
+            // Stop running animation
+            anim.SetBool("running", false);
         }
 
         // If the player hits the 'Space' key
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
         {
             // Change the velocity of the player to move up by 10
             // This mimics a jumping motion
